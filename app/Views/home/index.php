72f152d9207553d6f671d6ee651849ebc6f1872c
Ryan <ryanong14@gmail.com>
2023-06-06 16:46:27 +0800

    add links to home page

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,3 +1,5 @@
 <a href="<?php echo base_url('dashboard') ?>">Dashboard</a>
 <a href="<?php echo base_url('daily-pick') ?>">Daily Pick</a>
 <a href="<?php echo base_url('credits') ?>">Credits</a>
+<a href="<?php echo base_url('news') ?>">News</a>
+<a href="<?php echo base_url('trade') ?>">Trade</a>
